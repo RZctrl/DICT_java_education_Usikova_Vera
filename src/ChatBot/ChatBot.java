ec0eeb03a81bed74a29486fb533ef4b4d5981f9b
RZctrl <v.a.usikova@student.khai.edu>
2025-10-02 00:36:05 +0300

    ChatBot 2st st.

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,11 +1,21 @@
 package ChatBot;
 
+import java.util.Scanner;
+
 public class ChatBot {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String botName = "Botik";
         int birthYear = 2025;
 
         System.out.println("Hello! My name is " + botName + ".");
         System.out.println("I was created in " + birthYear + ".");
+        System.out.println("What's ur name?");
+
+        String userName = scanner.nextLine();
+
+        System.out.println("Nice to meet u, " + userName + "!");
+
+        scanner.close();
     }
 }
