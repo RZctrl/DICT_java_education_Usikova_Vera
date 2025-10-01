ebe57d17d14ba30463e568849271ebbbe4933e22
RZctrl <v.a.usikova@student.khai.edu>
2025-10-02 01:01:57 +0300

    ChatBot 5st.

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -8,10 +8,9 @@ public class ChatBot {
         String botName = "Botik";
         int birthYear = 2025;
 
-        System.out.println("Hello! My name is " + botName + ".");
+        System.out.println("Hello! My name's " + botName + ".");
         System.out.println("I was created in " + birthYear + ".");
         System.out.println("What's ur name?");
-
         String userName = scanner.nextLine();
 
         System.out.println("Nice to meet u, " + userName + "!");
@@ -25,12 +24,35 @@ public class ChatBot {
 
         System.out.println("Your age is " + age + "; that's a good time to take a nap!");
         System.out.println("Now I will prove to u that I can count to any number you want!");
-
         int number = scanner.nextInt();
 
         for (int i = 0; i <= number; i++) {
             System.out.println(i + "!");
         }
+
+        System.out.println("Hmm... Let's test your programming knowledge.");
+        System.out.println("Why are methods used in Java?");
+        System.out.println("1. To make code longer and more complicated.");
+        System.out.println("2. To repeat the same code multiple times.");
+        System.out.println("3. To organize code into reusable blocks.");
+        System.out.println("4. Because it's required by the compiler.");
+
+        int answer;
+        boolean correctAnswer = false;
+
+        while (!correctAnswer) {
+            answer = scanner.nextInt();
+            scanner.nextLine();
+
+            if (answer == 3) {
+                correctAnswer = true;
+                System.out.println("Congratulations, that's correct!");
+            } else {
+                System.out.println("Nope, try again.");
+            }
+        }
+
+        System.out.println("Goodbye, have a nice day!");
 
         scanner.close();
     }
