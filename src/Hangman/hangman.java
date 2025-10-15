011162245a14f81843c39cd1054aa6b9c10d81d9
RZctrl <v.a.usikova@student.khai.edu>
2025-10-15 22:40:25 +0300

    Hangman the 2st

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -1,8 +1,23 @@
 package Hangman;
 
+import java.util.Scanner;
+
 public class hangman {
     public static void main(String[] args) {
-        System.out.println("HANGMAN");
-        System.out.println("The game will be available soon.");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("HANGMAN:begin");
+        System.out.print("Guess the word: > ");
+        String guess = scanner.nextLine();
+
+        String secrWord = "java";
+
+        if (guess.equals(secrWord)) {
+            System.out.println("You survived!");
+        } else {
+            System.out.println("You lost!");
+        }
+
+        scanner.close();
     }
 }
