cceeb58f5f76524daf65af67778820447f62d36d
RZctrl <v.a.usikova@student.khai.edu>
2025-10-15 23:05:52 +0300

    Hangman the 5st

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -1,37 +1,80 @@
 package Hangman;
 
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class hangman {
     private static final String[] WORDS = {"python", "java", "javascript", "kotlin"};
+    private static final int MAX_ATTEMPTS = 8;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
         String secrWord = WORDS[random.nextInt(WORDS.length)];
 
-        StringBuilder hint = new StringBuilder();
-        for (int i = 0; i < secrWord.length(); i++) {
-            if (i < 2) {
-                hint.append(secrWord.charAt(i));
-            } else {
-                hint.append("-");
+        Set<Character> guessLetters = new HashSet<>();
+        int attemptsLeft = MAX_ATTEMPTS;
+
+
+
+
+        System.out.println("HANGMAN4");
+        System.out.println("You have only 8 attempts...");
+        System.out.println();
+
+
+        while (attemptsLeft > 0) {
+            displayWord(secrWord, guessLetters);
+
+            if (isWordGuessed(secrWord, guessLetters)) {
+                break;
             }
+
+            System.out.print("Input a letter: > ");
+            String input = scanner.nextLine().trim();
+
+            if (input.length() != 1) {
+                continue;
+            }
+
+            char letter = input.charAt(0);
+
+            if (secrWord.indexOf(letter) >= 0) {
+                guessLetters.add(letter);
+            } else {
+                System.out.println("No, that letter doesn't need here.");
+                attemptsLeft--;
+            }
+
+            System.out.println();
         }
 
-
-        System.out.println("HANGMAN2");
-        System.out.print("Guess the word " + hint + ": > ");
-        String guess = scanner.nextLine();
-
-
-        if (guess.equals(secrWord)) {
-            System.out.println("You survived!");
-        } else {
-            System.out.println("You lost again!");
-        }
+        System.out.println();
+        System.out.println("You won and survive!");
+        System.out.println("Thanks for playing!");
+        System.out.println();
+        System.out.println("We'll see how well you did in the next stage...");
 
         scanner.close();
+    }
+
+    private static void displayWord(String word, Set<Character> guessLetters) {
+        for (char c : word.toCharArray()) {
+            if (guessLetters.contains(c)) {
+                System.out.print(c);
+            } else {
+                System.out.print("-");
+            }
+        }
+        System.out.println();
+    }
+
+    private static boolean isWordGuessed(String word, Set<Character> guessLetters) {
+        for (char c : word.toCharArray()) {
+            if (!guessLetters.contains(c)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
