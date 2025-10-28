f5c4e4fd231b00757f40334894648214fdb1ea15
RZctrl <v.a.usikova@student.khai.edu>
2025-10-28 19:32:11 +0200

    TicTacToe 5st stage

--- src/TicTacToe/Game.java
+++ src/TicTacToe/Game.java
@@ -6,64 +6,83 @@ import java.util.*;
 public class Game {
     private GameBoard board;
     private GameAnalyzer analyzer;
-    private Scanner scanner;
+    private Scanner input;
+    private char activePlayer;
 
     public Game() {
         board = new GameBoard();
         analyzer = new GameAnalyzer(board);
-        scanner = new Scanner(System.in);
+        input  = new Scanner(System.in);
+        activePlayer  = 'X';
     }
 
     public void start() {
+        board.setupEmpty();
+        board.showBoard();
 
-        System.out.print("Enter cells: ");
-        String input = scanner.nextLine();
-        board.initializeFromString(input);
-        board.printFormattedBoard();
-
-        makeUserMove();
-        board.printFormattedBoard();
-    }
-
-    private void makeUserMove() {
         while (true) {
             System.out.print("Enter the coordinates: ");
-            String coordinates = scanner.nextLine();
+            String userInput  = input.nextLine();
 
-            try {
-                String[] parts = coordinates.split(" ");
-                if (parts.length != 2) {
-                    System.out.println("You should enter two numbers!");
-                    continue;
-                }
-
-                int row = Integer.parseInt(parts[0]);
-                int col = Integer.parseInt(parts[1]);
-
-                if (row < 1 || row > 3 || col < 1 || col > 3) {
-                    System.out.println("Coordinates should be from 1 to 3!");
-                    continue;
-                }
-
-
-                int boardRow = 3 - row;
-                int boardCol = col - 1;
+            if (!makeMove(userInput)) {
+                continue;
+            }
 
 
 
-                if (board.getCell(boardRow, boardCol) != '_') {
-                    System.out.println("This cell is occupied! Choose another one!");
-                    continue;
-                }
+            board.showBoard();
 
 
-
-                board.setCell(boardRow, boardCol, 'X');
+            String result  = analyzer.checkGameState();
+            if (!result.equals("Game not finished")) {
+                System.out.println(result);
                 break;
 
-            } catch (NumberFormatException e) {
-                System.out.println("You should enter numbers!");
             }
+
+
+            switchPlayer();
         }
+    }
+
+    private boolean makeMove(String userInput) {
+        try {
+            String[] parts = userInput.split(" ");
+            if (parts.length != 2) {
+                System.out.println("You should enter two numbers!");
+                return false;
+            }
+
+            int row = Integer.parseInt(parts[0]);
+            int col = Integer.parseInt(parts[1]);
+
+
+            if (row < 1 || row > 3 || col < 1 || col > 3) {
+                System.out.println("Coordinates should be from 1 to 3!");
+                return false;
+            }
+
+
+            int boardRow = row - 1;
+            int boardCol = col - 1;
+
+
+            if (board.getCell(boardRow, boardCol) != '_') {
+                System.out.println("This cell is occupied! Choose another one!");
+                return false;
+            }
+
+
+            board.setCell(boardRow, boardCol, activePlayer );
+            return true;
+
+        } catch (NumberFormatException e) {
+            System.out.println("You should enter numbers!");
+            return false;
+        }
+    }
+
+    private void switchPlayer() {
+        activePlayer  = (activePlayer  == 'X') ? 'O' : 'X';
     }
 }
--- src/TicTacToe/GameBoard.java
+++ src/TicTacToe/GameBoard.java
@@ -1,40 +1,50 @@
 package TicTacToe;
 
 public class GameBoard {
-    private char[][] grid;
+    private char[][] cells;
+
+
 
     public GameBoard() {
-        grid = new char[3][3];
+        cells = new char[3][3];
     }
 
 
-    public void initializeFromString(String input) {
-        if (input.length() != 9) {
+
+    public void setupEmpty() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                cells[i][j] = '_';
+            }
+        }
+    }
+
+
+    public void setupFromString(String startingPosition) {
+        if (startingPosition.length() != 9) {
             return;
         }
 
-        int index = 0;
+        int pos = 0;
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                char symbol = input.charAt(index++);
+                char symbol = startingPosition.charAt(pos++);
                 if (symbol != 'X' && symbol != 'O' && symbol != '_') {
                     symbol = '_';
                 }
-                grid[i][j] = symbol;
+                cells[i][j] = symbol;
             }
         }
     }
 
 
 
-
-
-    public void printFormattedBoard() {
+    public void showBoard() {
         System.out.println("---------");
         for (int i = 0; i < 3; i++) {
             System.out.print("| ");
             for (int j = 0; j < 3; j++) {
-                System.out.print(grid[i][j] + " ");
+                System.out.print(cells[i][j] + " ");
             }
             System.out.println("|");
         }
@@ -45,40 +55,35 @@ public class GameBoard {
 
     public char getCell(int row, int col) {
         if (row >= 0 && row < 3 && col >= 0 && col < 3) {
-            return grid[row][col];
+            return cells[row][col];
         }
         return ' ';
     }
 
-
-
     public void setCell(int row, int col, char value) {
         if (row >= 0 && row < 3 && col >= 0 && col < 3) {
-            grid[row][col] = value;
+            cells[row][col] = value;
         }
     }
-
-
 
 
     public int countSymbol(char symbol) {
-        int count = 0;
+        int total = 0;
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                if (grid[i][j] == symbol) {
-                    count++;
+                if (cells[i][j] == symbol) {
+                    total++;
                 }
             }
         }
-        return count;
+        return total;
     }
-
 
 
     public boolean hasEmptyCells() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                if (grid[i][j] == '_') {
+                if (cells[i][j] == '_') {
                     return true;
                 }
             }
@@ -86,26 +91,25 @@ public class GameBoard {
         return false;
     }
 
-
-
-    public boolean hasWinningLine(char symbol) {
-
+    public boolean hasWinner(char symbol) {
         for (int i = 0; i < 3; i++) {
-            if (grid[i][0] == symbol && grid[i][1] == symbol && grid[i][2] == symbol) {
+            if (cells[i][0] == symbol && cells[i][1] == symbol && cells[i][2] == symbol) {
                 return true;
             }
         }
+
 
         for (int j = 0; j < 3; j++) {
-            if (grid[0][j] == symbol && grid[1][j] == symbol && grid[2][j] == symbol) {
+            if (cells[0][j] == symbol && cells[1][j] == symbol && cells[2][j] == symbol) {
                 return true;
             }
         }
 
-        if (grid[0][0] == symbol && grid[1][1] == symbol && grid[2][2] == symbol) {
+
+        if (cells[0][0] == symbol && cells[1][1] == symbol && cells[2][2] == symbol) {
             return true;
         }
-        if (grid[0][2] == symbol && grid[1][1] == symbol && grid[2][0] == symbol) {
+        if (cells[0][2] == symbol && cells[1][1] == symbol && cells[2][0] == symbol) {
             return true;
         }
 
@@ -120,23 +124,23 @@ class GameAnalyzer {
         this.board = board;
     }
 
-    public String analyzeGameState() {
+    public String checkGameState() {
         int xCount = board.countSymbol('X');
         int oCount = board.countSymbol('O');
         int difference = Math.abs(xCount - oCount);
 
-        boolean xWins = board.hasWinningLine('X');
-        boolean oWins = board.hasWinningLine('O');
+        boolean xWon = board.hasWinner('X');
+        boolean oWon = board.hasWinner('O');
         boolean hasEmptyCells = board.hasEmptyCells();
 
-        if (difference >= 2 || (xWins && oWins)) {
+        if (difference >= 2 || (xWon && oWon)) {
             return "Impossible";
         }
 
-        if (xWins) {
+        if (xWon) {
             return "X wins";
         }
-        if (oWins) {
+        if (oWon) {
             return "O wins";
         }
 
