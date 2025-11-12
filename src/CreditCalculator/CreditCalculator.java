2cab4c84a349aee7c90c1e7c38e13672ad47ce32
RZctrl <v.a.usikova@student.khai.edu>
2025-11-12 14:45:05 +0200

    CreditCalculator 2st stage

--- src/CreditCalculator/CreditCalculator.java
+++ src/CreditCalculator/CreditCalculator.java
@@ -1,11 +1,60 @@
 package CreditCalculator;
 
+import java.util.*;
+
 public class CreditCalculator {
-    public static void main(String[] args) {
-        System.out.println("Loan principal: 1000");
-        System.out.println("Month 1: repaid 250");
-        System.out.println("Month 2: repaid 250");
-        System.out.println("Month 3: repaid 500");
-        System.out.println("The loan has been repaid!");
+    private Scanner scanner;
+
+    public CreditCalculator() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void begin() {
+        System.out.println("Enter the loan principal:");
+        int principal = scanner.nextInt();
+
+        System.out.println("What do you want to calculate?");
+        System.out.println("type \"m\" - for number of monthly payments,");
+        System.out.println("type \"p\" - for the monthly payment:");
+        scanner.nextLine();
+        String choice = scanner.nextLine();
+
+        switch (choice) {
+            case "m":
+                calcMonths(principal);
+                break;
+            case "p":
+                calcPayment(principal);
+                break;
+            default:
+                System.out.println("Invalid.");
+        }
+    }
+
+    private void calcMonths(int principal) {
+        System.out.println("Enter the monthly payment:");
+        int payment = scanner.nextInt();
+
+        if (payment >= principal) {
+            System.out.println("It will take 1 month to repay the loan");
+        } else {
+            int months = (int) Math.ceil((double) principal / payment);
+            System.out.println("It will take " + months + " months to repay the loan");
+        }
+    }
+
+    private void calcPayment(int principal) {
+        System.out.println("Enter the number of months:");
+        int months = scanner.nextInt();
+
+        double basePayment = (double) principal / months;
+        int payment = (int) Math.ceil(basePayment);
+
+        if (principal % months == 0) {
+            System.out.println("Your monthly payment = " + payment);
+        } else {
+            int lastPayment = principal - payment * (months - 1);
+            System.out.println("Your monthly payment = " + payment + " and the last payment = " + lastPayment);
+        }
     }
 }
---
+++ src/CreditCalculator/LaunchCalculator.java
@@ -0,0 +1,10 @@
+package CreditCalculator;
+
+
+public class LaunchCalculator {
+    public static void main(String[] args) {
+        CreditCalculator calculator = new CreditCalculator();
+
+        calculator.begin();
+    }
+}
